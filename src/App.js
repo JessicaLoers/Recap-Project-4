b3e7547755444cb6fa107b82733a63c99cac11e2
Jessica Lörs <jessica.loers@gmail.com>
2024-01-04 14:29:40 +0100

    implement local storage hook and add comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { useState } from 'react';
 import { uid } from 'uid';
+// Custom hook for managing state in local storage.
+import useLocalStorageState from 'use-local-storage-state';
 
+// Importing the List component.
 import List from './components/List';
 import Form from './components/Form';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  // 'useLocalStorageState' is a custom hook that manages state in the browser's local storage.
+  // '_ACTIVITIES' is the key under which activities will be stored in local storage.
+  // The default value for activities state is an empty array.
+  const [activities, setActivities] = useLocalStorageState('_ACTIVITIES', { defaultValue: [] });
 
   function handleAddActivity(newActivity) {
     setActivities((prevActivities) => [...prevActivities, { id: uid(), ...newActivity }]);
@@ -13,6 +18,7 @@ function App() {
 
   return (
     <main>
+      {/* The List component is passed the activities state as a prop*/}
       <List activities={activities} />
       <Form onAddActivity={handleAddActivity} />
     </main>
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,12 +1,16 @@
 import './List.css';
 
+// The List components receives "activities" as a prop, which is an array of activity objects
 export default function List({ activities }) {
   return (
     <>
       <h2>Activities</h2>
+      {/* Mapping over each activity in the "activities" array to create list items */}
       <ul className="list">
+        {/* Each activity is destructured to get "name" and "id" properties. */}
         {activities.map(({ name, id }) => {
           return (
+            // "key" prop is important for React to handle re-renders efficiently.
             <li key={id} className="list__item">
               {name}
             </li>
