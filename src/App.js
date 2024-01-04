bff4f78649958f17c053dda0e5b8b5db11db157a
Jessica Lörs <jessica.loers@gmail.com>
2024-01-04 14:36:03 +0100

    remove comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import { uid } from 'uid';
-// Custom hook for managing state in local storage.
 import useLocalStorageState from 'use-local-storage-state';
 
-// Importing the List component.
 import List from './components/List';
 import Form from './components/Form';
 
 function App() {
-  // 'useLocalStorageState' is a custom hook that manages state in the browser's local storage.
-  // '_ACTIVITIES' is the key under which activities will be stored in local storage.
-  // The default value for activities state is an empty array.
   const [activities, setActivities] = useLocalStorageState('_ACTIVITIES', { defaultValue: [] });
 
   function handleAddActivity(newActivity) {
@@ -18,7 +13,6 @@ function App() {
 
   return (
     <main>
-      {/* The List component is passed the activities state as a prop*/}
       <List activities={activities} />
       <Form onAddActivity={handleAddActivity} />
     </main>
