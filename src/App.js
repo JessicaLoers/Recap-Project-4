b516c4a01fb95cf443de5e8fb8c6dbe2653f5e4a
Jessica Lörs <jessica.loers@gmail.com>
2024-01-04 14:07:42 +0100

    remove comments

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import Form from './components/Form';
 function App() {
   const [activities, setActivities] = useState([]);
 
-  // This function updates the "activities" state variable by adding a new activity to the existing list
   function handleAddActivity(newActivity) {
-    // Call 'setActivities' (state setter function)
-    // Spread syntax ""...prevActivities" is used to copy all previous activities
-    // Generates a unique identifier for the new activity using uid() function
-    // Spreads the properties of "newActivity" into this new object.
     setActivities((prevActivities) => [...prevActivities, { id: uid(), ...newActivity }]);
   }
 
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,27 +4,18 @@ export default function Form({ onAddActivity }) {
   function handleSubmit(event) {
     event.preventDefault();
 
-    // Accesses the form element that triggered the submit event.
     const form = event.target;
 
-    // Destructures "name" and "isForGoodWeather" from the form's elements collection
-    // This allows direct access to these specific form fields
     const { name, isForGoodWeather } = form.elements;
 
-    // Constructs a new object 'newEntry' with properties:
-    // - "name": the value (text) entered in the "name" input field
-    // - "isForGoodWeather": the boolean checked state of the "isForGoodWeather" checkbox
     const newEntry = {
       name: name.value,
       isForGoodWeather: isForGoodWeather.checked,
     };
 
-    // Calls "onAddActivity" function with "newEntry" as an argument
     onAddActivity(newEntry);
 
-    // Resets the form to its initial state, clearing all input fields
     form.reset();
-    // Sets the focus back to the "name" input field
     name.focus();
   }
 
