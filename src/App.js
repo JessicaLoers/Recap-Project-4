c2009262cf1fd724f4889002ec70a4e97fff9d89
Jessica Lörs <jessica.loers@gmail.com>
2024-01-04 14:51:39 +0100

    remove comments

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ import Form from './components/Form';
 function App() {
   const [activities, setActivities] = useLocalStorageState('_ACTIVITIES', { defaultValue: [] });
 
-  // A constant to simulate whether the weather is good or not.
   const isGoodWeather = false;
 
-  // Filters activities based on "isGoodWeather"
   const filteredActivities = activities.filter((activity) => {
     return isGoodWeather ? activity.isForGoodWeather : !activity.isForGoodWeather;
   });
@@ -21,7 +19,6 @@ function App() {
 
   return (
     <main>
-      {/*List component is passed the filtered Activities*/}
       <List activities={filteredActivities} isGoodWeather={isGoodWeather} />
       <Form onAddActivity={handleAddActivity} />
     </main>
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,7 +3,6 @@ import './List.css';
 export default function List({ activities, isGoodWeather }) {
   return (
     <>
-      {/* Checks the value of the "isGoodWeather" variable to dynamically display the text*/}
       <h2>{isGoodWeather ? 'good' : 'bad'} Weather Activities</h2>
       <ul className="list">
         {activities.map(({ name, id }) => {
